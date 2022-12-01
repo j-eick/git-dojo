b9445ed0e736f247dfca50c193d3c65c1eae6689
Joschka Eickhoff <jeickhoff@posteo.de>
2022-12-01 16:52:48 +0100

    cleaned up

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Link from "next/link"
-
-Link
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <Link href="/test1">test1</Link> 
+      <Link href="/test1">test1</Link>
     </>
-  )
+  );
 }
